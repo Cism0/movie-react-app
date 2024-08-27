8e492ded07834e35463ac3fd05f2f9022ce00b68
Your Name <you@example.com>
2024-08-27 06:23:34 -0400

    I fixed a massive failure of an interaction in which the user could not search for a movie if they had already searched for one movie. Stylized the button and changed the website's font to be a more appropriate.

--- src/App.js
+++ src/App.js
@@ -1,70 +1,3 @@
-// 1
-
-// import React, { useState } from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import SearchBar from './components/SearchBar';
-// import MovieList from './components/MovieList';
-// import MovieInfo from './components/MovieInfo';
-// import Navbar from './components/Navbar';
-// import './App.css';
-
-// function App() {
-//   const [movies, setMovies] = useState([]);
-
-//   const fetchMovies = async (searchTerm) => {
-//     const response = await fetch(`https://omdbapi.com/?s=${searchTerm}&page=1&apikey=fc1fef96`);
-//     const data = await response.json();
-//     if (data.Response === "True") {
-//       setMovies(data.Search.slice(0, 6));
-//     }
-//   };
-
-//   return (
-//     <Router>
-//       <div className="wrapper">
-//         <Navbar />
-//         <SearchBar onSearch={fetchMovies} />
-//         <Routes>
-//           <Route path="/" element={<MovieList movies={movies} />} />
-//           <Route path="/movie/:id" element={<MovieInfo />} />
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-// 2
-
-// import React, { useState } from 'react';
-// import SearchBar from './components/SearchBar';
-// import MovieList from './components/MovieList';
-// import MovieInfo from './components/MovieInfo';
-// import Navbar from './components/Navbar';
-// import useMovies from './hooks/useMovies';
-// import './App.css';
-
-// function App() {
-//   const { movies, selectedMovie, fetchMovies, fetchMovieDetails, loading } = useMovies();
-
-//   return (
-//     <div className="wrapper">
-//       <Navbar />
-//       <main>
-//         <SearchBar onSearch={fetchMovies} />
-//         {loading && <div className="spinner">Loading...</div>}
-//         <MovieList movies={movies} onSelectMovie={fetchMovieDetails} />
-//         {selectedMovie && <MovieInfo movie={selectedMovie} />}
-//       </main>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import SearchBar from './components/SearchBar';
@@ -85,8 +18,24 @@ function App() {
           <SearchBar onSearch={fetchMovies} />
           {loading && <div className="spinner">Loading...</div>}
           <Routes>
-            <Route path="/" element={<MovieList movies={movies} onSelectMovie={fetchMovieDetails} />} />
-            <Route path="/movie/:id" element={<MovieInfo movie={selectedMovie} />} />
+            <Route 
+              path="/" 
+              element={
+                <>
+                  <MovieList movies={movies} onSelectMovie={fetchMovieDetails} />
+                  {selectedMovie && <MovieInfo movie={selectedMovie} />}
+                </>
+              } 
+            />
+            <Route 
+              path="/movie/:id" 
+              element={
+                <>
+                  {movies.length > 0 && <MovieList movies={movies} onSelectMovie={fetchMovieDetails} />}
+                  <MovieInfo movie={selectedMovie} />
+                </>
+              } 
+            />
           </Routes>
         </main>
       </div>
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,32 +1,3 @@
-// import React from 'react';
-
-// const MovieList = ({ movies, onSelectMovie }) => {
-//   return (
-//     <div className="result-grid">
-//       {movies.map((movie) => (
-//         <div 
-//           key={movie.imdbID} 
-//           className="search-list-item" 
-//           onClick={() => onSelectMovie(movie.imdbID)}
-//         >
-//           <div className="search-item-thumbnail font-color-white">
-//             <img 
-//               src={movie.Poster !== "N/A" ? movie.Poster : "image_not_found.png"} 
-//               alt={movie.Title} 
-//             />
-//           </div>
-//           <div className="search-item-info">
-//             <h3>{movie.Title}</h3>
-//             <p>{movie.Year}</p>
-//           </div>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
-
-// export default MovieList;
-
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
